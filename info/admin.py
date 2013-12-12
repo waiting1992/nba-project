1059d65f053aea8593662fea6eb4768985c900c1
chenglei wu <wuchenglei1993@gmail.com>
2013-12-12 18:03:16 +0800

    some changes in models.py

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from info.models import Player, Team, Coach
+
+admin.site.register(Player)
+admin.site.register(Coach)
+admin.site.register(Team)
